d3baf9e42896994864d0fcf34c0afa3603e1b84d
Rimantas <belauzas@gmail.com>
2024-07-04 12:32:23 +0300

    Pikachu

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log('labas');
 console.log('labas');
 console.log('labas');
 console.log('labas');
+
+const pokeUrl = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pokémon_Pikachu_art.png';
+const mainDOM = document.querySelector('main');
+mainDOM.innerHTML = `<img src="${pokeUrl}" alt="Poke poke">`;
